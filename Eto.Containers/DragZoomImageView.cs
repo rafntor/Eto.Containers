4ab58ce226163242e6b157ef7e5426395243a498
Tore Fuglestad <tore@fuglestad.no>
2022-03-06 09:12:50 +0100

    add zoom to selection

--- Eto.Containers/DragZoomImageView.cs
+++ Eto.Containers/DragZoomImageView.cs
@@ -10,6 +10,7 @@ namespace Eto.Containers
 	public class DragZoomImageView : Drawable
 	{
 		Image? _image;
+		RectangleF _selectRectangle;
 		IMatrix _base_transform = Matrix.Create();
 		IMatrix _size_transform = Matrix.Create();
 		IMatrix _zoom_transform = Matrix.Create();
@@ -17,6 +18,11 @@ namespace Eto.Containers
 		public Cursor DragCursor { get; set; } = Cursors.Move;
 		public Keys DragModifier { get; set; } = Keys.None;
 		public MouseButtons DragButton { get; set; } = MouseButtons.Primary;
+		public Cursor ZoomCursor { get; set; } = Cursors.Pointer;
+		public Keys ZoomModifier { get; set; } = Keys.None;
+		public MouseButtons ZoomButton { get; set; } = MouseButtons.Alternate;
+		public Color ZoomColor { get; set; } = new Color(Colors.Yellow, 0.3f);
+
 		public Image? Image
 		{
 			get => _image;
@@ -45,17 +51,15 @@ namespace Eto.Containers
 		{
 			if (Image != null && Width > 0 && Height > 0)
 			{
-				var scale_x = (float)Width / Image.Width;
-				var scale_y = (float)Height / Image.Height;
-				var scale = Math.Min(scale_x, scale_y);
+				var scale = (SizeF) Size / Image.Size;
+				scale.Width = scale.Height = Math.Min(scale.Width, scale.Height);
 
-				var xoff = Width - scale * Image.Width;
-				var yoff = Height - scale * Image.Height;
+				var offset = (PointF) (Size - scale * Image.Size) / 2;
 
-				_base_transform = Matrix.FromTranslation(xoff / 2, yoff / 2);
+				_base_transform = Matrix.FromTranslation(offset);
 				_base_transform.Scale(scale);
 
-				_size_transform = Matrix.FromScale(scale, scale).Inverse();
+				_size_transform = Matrix.FromScale(scale).Inverse();
 			}
 		}
 		protected override void OnSizeChanged(EventArgs e)
@@ -77,20 +81,47 @@ namespace Eto.Containers
 					throw new ArgumentException("Use Image Property ; not Content");
 			}
 		}
+		private bool ZoomMode
+		{
+			get => !_selectRectangle.IsZero;
+			set 
+			{
+				if (value)
+					_selectRectangle.TopLeft = _selectRectangle.BottomRight + 1;
+				else
+					_selectRectangle = RectangleF.Empty;
+			}
+		}
+
 		protected override void OnPaint(PaintEventArgs e)
 		{
 			if (Image != null)
 			{
+				e.Graphics.SaveTransform();
 				e.Graphics.MultiplyTransform(_base_transform);
 				e.Graphics.MultiplyTransform(_zoom_transform);
 				e.Graphics.DrawImage(Image, PointF.Empty);
+
+				if (ZoomMode)
+				{
+					e.Graphics.RestoreTransform();
+
+					var zoom_pen = Pens.Black;
+					zoom_pen.DashStyle = DashStyles.Dash;
+
+					e.Graphics.FillRectangle(ZoomColor, _selectRectangle);
+					e.Graphics.DrawRectangle(zoom_pen, _selectRectangle);
+				}
 			}
 			base.OnPaint(e);
 		}
 		protected override void OnMouseWheel(MouseEventArgs e)
 		{
 			var scale_siz = SizeF.Empty + 1 + e.Delta.Height / 6;
-			var scale = Matrix.FromScaleAt(scale_siz, e.Location);
+
+			var location = _base_transform.Inverse().TransformPoint(e.Location);
+
+			var scale = Matrix.FromScaleAt(scale_siz, location);
 
 			Matrix.Append(_zoom_transform, scale);
 
@@ -104,16 +135,18 @@ namespace Eto.Containers
 
 			base.OnMouseDoubleClick(e);
 		}
-		#region mouse pan
+#region mouse pan
 		PointF _mouse_down;
 		protected override void OnMouseDown(MouseEventArgs e)
 		{
-			if (e.Buttons == DragButton && e.Modifiers == DragModifier)
-			{
+			ZoomMode = e.Buttons == ZoomButton && (ZoomModifier == Keys.None || e.Modifiers == ZoomModifier);
+
+			bool dragMode = e.Buttons == DragButton && (DragModifier == Keys.None || e.Modifiers == DragModifier);
+
+			if (dragMode || ZoomMode)
 				_mouse_down = e.Location;
 
-				_defaultCursor = Cursor;
-			}
+			_defaultCursor = Cursor;
 
 			base.OnMouseDown(e);
 		}
@@ -121,17 +154,27 @@ namespace Eto.Containers
 		{
 			if (_mouse_down != PointF.Empty)
 			{
-				Cursor = DragCursor;
+				if (ZoomMode)
+				{
+					Cursor = ZoomCursor;
 
-				var dist = e.Location - _mouse_down;
+					_selectRectangle.TopLeft = PointF.Min(e.Location, _mouse_down);
+					_selectRectangle.BottomRight = PointF.Max(e.Location, _mouse_down);
+				}
+				else
+				{
+					Cursor = DragCursor;
 
-				dist = _size_transform.TransformPoint(dist);
+					var dist = e.Location - _mouse_down;
 
-				var move = Matrix.FromTranslation(dist);
+					dist = _size_transform.TransformPoint(dist);
 
-				Matrix.Append(_zoom_transform, move);
+					var move = Matrix.FromTranslation(dist);
 
-				_mouse_down = e.Location;
+					Matrix.Append(_zoom_transform, move);
+
+					_mouse_down = e.Location;
+				}
 
 				this.Invalidate();
 			}
@@ -142,6 +185,22 @@ namespace Eto.Containers
 		{
 			if (_mouse_down != PointF.Empty)
 			{
+				if (ZoomMode)
+				{
+					var scale_siz = Size / _selectRectangle.Size;
+					scale_siz.Width = scale_siz.Height = Math.Min(scale_siz.Width, scale_siz.Height);
+
+					var location = _base_transform.Inverse().TransformPoint(_selectRectangle.Center);
+
+					var scale = Matrix.FromScaleAt(scale_siz, location);
+
+					Matrix.Append(_zoom_transform, scale);
+
+					this.Invalidate();
+
+					ZoomMode = false;
+				}
+
 				_mouse_down = PointF.Empty;
 
 				Cursor = _defaultCursor;
@@ -149,6 +208,6 @@ namespace Eto.Containers
 
 			base.OnMouseUp(e);
 		}
-		#endregion
+#endregion
 	}
 }
